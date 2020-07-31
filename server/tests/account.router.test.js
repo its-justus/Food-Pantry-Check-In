58671bf3c8cec63edd78a6dba3ff1fc5909c159e
Luke Johnson <lukejohnson3712@gmail.com>
2020-07-31 12:02:54 -0500

    Add account router.

--- server/tests/account.router.test.js
+++ server/tests/account.router.test.js
@@ -4,7 +4,7 @@ const testServer = require('supertest');
 describe('Test the account path', () => {
   it('It should respond 200 the account owner', async (done) => {
     const response = await testServer(app).get('/api/account');
-    // expect(response.statusCode).toBe(200);
+    expect(response.statusCode).toBe(200);
     done();
   });
 });
