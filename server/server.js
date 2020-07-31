e53cefeee17de1d97db8adc84082e15247da32da
Bailey Niemiller <baileyniemiller@gmail.com>
2020-07-31 15:00:53 -0500

    added env file, and got one order router test passing

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const locationRouter = require('./routes/location.router');
 const accountRouter = require('./routes/account.router');
+const orderRouter = require('./routes/order.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -13,5 +14,6 @@ app.use(express.static('build'));
 
 app.use('/api/location', locationRouter);
 app.use('/api/account', accountRouter);
+app.use('/api/order', orderRouter);
 
 module.exports = app;
