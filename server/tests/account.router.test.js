92b853088d670bb7e195e7e9e931f67554463a0e
Luke Johnson <lukejohnson3712@gmail.com>
2020-07-31 11:46:04 -0500

    Stop infinate test.

--- server/tests/account.router.test.js
+++ server/tests/account.router.test.js
@@ -2,8 +2,9 @@ const app = require('../server');
 const testServer = require('supertest');
 
 describe('Test the account path', () => {
-  test('It should respond 200 the account owner', async () => {
+  it('It should respond 200 the account owner', async done => {
     const response = await testServer(app).get('/api/account');
     expect(response.statusCode).toBe(200);
+    done();
   });
 });
