b880a3b417703a5ef61755af18f43b143b02e553
Luke Johnson <lukejohnson3712@gmail.com>
2020-07-31 11:19:04 -0500

    Basic server.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,20 @@
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
 
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('build'));
+
+const accountRouter = require('./routes/account.router');
+
+app.use('/api/account', accountRouter);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
 
 module.exports = app;
