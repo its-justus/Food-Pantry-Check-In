737c33721c328dec10deda6114a899f8ba67ea44
Luke Johnson <lukejohnson3712@gmail.com>
2020-08-03 11:27:38 -0500

    Log url.

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -3,6 +3,7 @@ const url = require('url');
 
 let config = {};
 
+console.log('process.env.DATABASE_URL', process.env.DATABASE_URL);
 if (process.env.DATABASE_URL) {
   // Heroku gives a url, not a connection object:
   // https://github.com/Fetchinator7/node-pg-pool
