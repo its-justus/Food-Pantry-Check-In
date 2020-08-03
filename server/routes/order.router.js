1b7f2749085d1d33917d5425cc37ddf3f3ada781
Bailey Niemiller <baileyniemiller@gmail.com>
2020-08-03 09:44:34 -0500

    passing route tests

--- server/routes/order.router.js
+++ server/routes/order.router.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const pool = require("../modules/pool");
 
+// GET to /api/order
 router.get("/", (req, res) => {
-  res.sendStatus(200);
+  res.status(200).send(req.body);
 });
+// end GET
+
+// POST api/order
+router.post("/", (req, res) => {
+  res.status(201).send(req.body);
+});
+// end POST
+
+// PUT /api/order/id
+router.put("/1", (req, res) => {
+  res.status(200).send(req.body);
+});
+// end PUT
+
+// DELETE to /api/order/:id
+router.delete("/1", (req, res) => {
+  res.status(204).send(req.body);
+});
+// end DELETE
 
 module.exports = router;
