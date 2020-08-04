702477744e95bfb92666261518ad849663b5d707
Luke Johnson <lukejohnson3712@gmail.com>
2020-08-04 15:27:39 -0500

    Fix Passport.js to close #74.

--- server/server.js
+++ server/server.js
@@ -12,6 +12,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('build'));
 
+const sessionMiddleware = require('./modules/session-middleware');
+const passport = require('./strategies/user.strategy');
+
+// Passport Session Configuration
+app.use(sessionMiddleware);
+
+// Start up passport sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/api/location', locationRouter);
 app.use('/api/account', accountRouter);
 app.use('/api/order', orderRouter);
--- server/strategies/user.strategy.js
+++ server/strategies/user.strategy.js
@@ -4,14 +4,14 @@ const encryptLib = require('../modules/encryption');
 const pool = require('../modules/pool');
 
 passport.serializeUser((user, done) => {
-  done(null, user.user_id);
+  done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-  pool.query('SELECT * FROM "users" WHERE user_id = $1', [id]).then((result) => {
+  pool.query('SELECT * FROM "account" WHERE id = $1', [id]).then((result) => {
     // Handle Errors
     const user = result && result.rows && result.rows[0];
-
+    
     if (user) {
       // user found
       delete user.password; // remove password so it doesn't get sent
@@ -33,7 +33,7 @@ passport.deserializeUser((id, done) => {
 
 // Does actual work of logging in
 passport.use('local', new LocalStrategy((username, password, done) => {
-  pool.query('SELECT * FROM "users" WHERE user_name = $1', [username])
+  pool.query('SELECT * FROM "account" WHERE email = $1', [username])
     .then((result) => {
       const user = result && result.rows && result.rows[0];
       if (user && encryptLib.comparePassword(password, user.password)) {
