237bb67d9e3005558e368a66792c57963525cb8c
Luke Johnson <lukejohnson3712@gmail.com>
2020-08-04 15:57:44 -0500

    Fix warnings.

--- server/tests/account.router.test.js
+++ server/tests/account.router.test.js
@@ -1,5 +1,5 @@
 const app = require('../server');
-const testServer = require('supertest')(app);
+const request = require('supertest')(app);
 
 // describe('Test the account path', () => {
 //   it('It should respond 200 the account owner', async (done) => {
@@ -9,25 +9,24 @@ const testServer = require('supertest')(app);
 //   });
 // });
 
-// describe('POST /api/account', () => {
-//   it('responds with account POST query result', async (done) => {
-//     const res = await testServer
-//       .post('/api/account')
-//       .send({
-//         name: 'name',
-//         email: 'email',
-//         password: 'password'
-//       })
-//       .expect(200);
-//     console.log(res.body);
-//     // expect(res.body).toHaveProperty({
-//     //   name: 'name',
-//     //   email: 'email',
-//     //   password: 'password'
-//     // });
-//     done();
-//   });
-// });
+describe('POST /api/account', () => {
+  it('responds with account POST query result', async (done) => {
+    const res = await request
+      .post('/api/account/login')
+      .send({
+        username: 'email',
+        password: 'password'
+      })
+      .expect(200);
+    console.log(res.body);
+    // expect(res.body).toHaveProperty({
+    //   name: 'name',
+    //   email: 'email',
+    //   password: 'password'
+    // });
+    done();
+  });
+});
 
 test('app exists', () => {
   expect(true).toBe(true);
