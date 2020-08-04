aff98d27c9c15b33e4316c8bcbaf57185a7f6a18
Luke Johnson <lukejohnson3712@gmail.com>
2020-08-04 16:00:45 -0500

    Add dependency.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 // import LogOutButton from "../LogOutButton/LogOutButton"
 import './Header.css';
