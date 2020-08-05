aaf1cbe5b00c267e10b61b8bf957b93ab9fce65d
Bailey Niemiller <baileyniemiller@gmail.com>
2020-08-05 15:14:14 -0500

    coming together. color scheme picked out and navbar responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,23 @@ import { Link, NavLink } from "react-router-dom";
 import { connect } from "react-redux";
 import LogOutButton from "../LogOutButton/LogOutButton"
 import Navbar from "react-bootstrap/Navbar";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 import './Header.css';
 
 const Header = (props) => (
-  <header id="header">
-    <h1 className="navTitle">Emergency Food Pantry</h1>
-  </header>
+  <AppBar position="static" id="navBar">
+    <Toolbar>
+      <IconButton edge="start" aria-label="menu">
+        <MenuIcon style={{ fill: "#faaf46" }} />
+      </IconButton>
+      <Typography id="navTitle">Emergency Food Pantry</Typography>
+    </Toolbar>
+  </AppBar>
 );
 
 const mapStateToProps = (state) => ({
