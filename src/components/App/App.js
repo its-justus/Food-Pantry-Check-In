c5aa0d31463d83b7ac2e2fee56c1df92aced9066
Daniel Simango <daniel.simmz@gmail.com>
2020-08-05 08:36:22 -0500

    added a little styling to component dashboard

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,28 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import {
   HashRouter as Router,
   Route,
-  Redirect,
-  Switch,
 } from "react-router-dom";
 import { connect } from "react-redux";
-import RegisterPage from '../RegisterPage/RegisterPage';
-import Header from '../Header/Header'
-import Dashboard from '../Dashboard/Dashboard';
+import RegisterPage from "../RegisterPage/RegisterPage";
+import Header from "../Header/Header";
+import Dashboard from "../Dashboard/Dashboard";
 
 function App() {
   return (
     <Router>
-      <Route exact path="/">
-        <div className="App">
+      <div className="App">
+        <Route exact path="/">
           <Header />
           <RegisterPage />
-        </div>
-      </Route>
-      <Route exact path="/dashboard">
-        <Dashboard />
-      </Route>
+        </Route>
+        <Route exact path="/home">
+          <Dashboard />
+        </Route>
+      </div>
     </Router>
   );
 }
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -5,41 +5,55 @@ class Dashboard extends Component {
   render() {
     return (
       <>
-        <div className="dashBoard">
-          <form>
-            <ul>
-              <li>Bob checked in 3 mins ago.</li>
-              <li>Linda checked in 10 mins ago.</li>
-            </ul>
-          </form>
-          <form>
-            <header className="header">
-              <h3>Bob Smith</h3>
-              <h4>ID: ABCD1234</h4>
-            </header>
-            <body id="dashBody">
-              <span>
-                <b>Food Restrictions</b>
-              </span>
-              <span>
-                Bobby snobby eats everything. He's like a marauding Komodo
-                dragon when it comes to food. Give him everything, he can handle
-                it all.
-              </span>
-              <span>Walking home: No</span>
-              <span>Child Birthday coming up: No</span>
-              <span>Someone at home is pregnant: Yes</span>
-              <button>Check in</button>
-              <button>Eligible</button>
-            </body>
-          </form>
-          <form>
-            <ul>
-              <li>Sarah received food today.</li>
-              <li>James received food today.</li>
-              <li>David received food today.</li>
-            </ul>
-          </form>
+        <div class="dashBoard row">
+          <div class="span6">
+            <form>
+              <ul>
+                <li>Bob checked in 3 mins ago.</li>
+                <li>Linda checked in 10 mins ago.</li>
+              </ul>
+            </form>
+          </div>
+          <div class="span6">
+            <form>
+              <header className="header">
+                <h3>Bob Smith</h3>
+                <h4>ID: ABCD1234</h4>
+              </header>
+              <body id="dashBody">
+                <span>
+                  <b>Food Restrictions</b>
+                </span>
+                <span>
+                  Bobby snobby eats everything. He's like a marauding Komodo
+                  dragon when it comes to food. Give him everything, he can
+                  handle it all.
+                </span>
+                <br />
+                <span>Walking home: No</span>
+                <br />
+                <span>Child Birthday coming up: No</span>
+                <br />
+                <span>Someone at home is pregnant: Yes</span>
+                <br />
+                <button class="btn btn-large btn-primary" type="submit">
+                  Check in
+                </button>
+                <button class="btn btn-large btn-primary" type="submit">
+                  Eligible
+                </button>
+              </body>
+            </form>
+          </div>
+          <div class="span6">
+            <form>
+              <ul>
+                <li>Sarah received food today.</li>
+                <li>James received food today.</li>
+                <li>David received food today.</li>
+              </ul>
+            </form>
+          </div>
         </div>
       </>
     );
