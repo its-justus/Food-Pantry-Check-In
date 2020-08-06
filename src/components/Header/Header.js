6358f0d23bb382accf9813fe892a99f51117c699
Bailey Niemiller <baileyniemiller@gmail.com>
2020-08-06 10:36:49 -0500

    getting rid of warnings

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,10 +7,8 @@ import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import Drawer from "@material-ui/core/Drawer";
-import Button from "@material-ui/core/Button";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import "./Header.css";
 
