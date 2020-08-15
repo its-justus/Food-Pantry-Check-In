2135eaf3cd9afef685c726901ad5ca969381c12e
Luke Johnson <lukejohnson3712@gmail.com>
2020-08-15 18:40:00 -0500

    Fix tests.

--- server/tests/location.router.test.js
+++ server/tests/location.router.test.js
@@ -1,6 +1,5 @@
 const app = require('../server');
 const request = require('supertest');
-const pool = require('../modules/pool');
 const users = require('./testUsers');
 
 const adminUser = request.agent(app);
@@ -10,19 +9,22 @@ const adminPassword = adminInfo.adminPassword;
 
 const locationID = users.locationID;
 const locationDescription = 'order test location';
+const newLocationDescription = 'New description.';
+const locationObject = {
+  id: locationID,
+  description: locationDescription
+};
+const newLocationObject = {
+  id: locationID,
+  description: newLocationDescription
+};
 
-const testUser = request.agent(app);
-const testUserInfo = users.testUser;
-const testUserID = testUserInfo.testUserID;
-const testUserName = testUserInfo.testUserName;
-const testUserEmail = testUserInfo.testUserEmail;
-const testUserPassword = testUserInfo.testUserPassword;
-
-// afterAll(async (done) => {
-//   await pool.query(`DELETE FROM order WHERE location_id = ${locationID};`);
-//   await pool.query(`DELETE FROM location WHERE location_id = ${locationID};`);
-//   done();
-// });
+// const testUser = request.agent(app);
+// const testUserInfo = users.testUser;
+// const testUserID = testUserInfo.testUserID;
+// const testUserName = testUserInfo.testUserName;
+// const testUserEmail = testUserInfo.testUserEmail;
+// const testUserPassword = testUserInfo.testUserPassword;
 
 describe('POST to login /api/account/login', () => {
   it("responds with the administrator's information", async (done) => {
@@ -55,90 +57,70 @@ describe('GET /api/account/', () => {
   });
 });
 
-//   describe(`GET /api/account/${newUserId}`, () => {
-//     it('Get info for the newly added client', async (done) => {
-//       const res = await adminUser
-//         .get(`/api/account/${newUserId}`)
-//         .expect(200);
-//       expect(res.body).toEqual({
-//         id: newUserId,
-//         name: newUserName,
-//         email: newUserEmail,
-//         access_level: 1
-//       });
-//       done();
-//     });
-//   });
+describe('GET /api/location/', () => {
+  it('Responds with an array of locations.', async (done) => {
+    const res = await adminUser
+      .get('/api/location')
+      .expect(200);
+    expect(res.body).toEqual(
+      expect.arrayContaining([expect.any(Object)])
+    );
+    done();
+  });
+});
 
-//   describe(`DELETE /api/account/${newUserId}`, () => {
-//     it('This is the last test so delete the newly added client', async (done) => {
-//       await adminUser
-//         .delete(`/api/account/${newUserId}`)
-//         .expect(204);
-//       done();
-//     });
-//   });
-// });
+// Start with posting a new location
+describe('POST /api/location', () => {
+  it('Posts the new location', async (done) => {
+    const res = await adminUser
+      .post('/api/location')
+      .send(locationObject)
+      .expect(201);
+    expect(res.body).toEqual(locationObject);
+    done();
+  });
+});
 
+describe(`PUT /api/location/${locationID}`, () => {
+  it('Responds with the updated array.', async (done) => {
+    const res = await adminUser
+      .put(`/api/location/${locationID}`)
+      .send(newLocationObject)
+      .expect(200);
+    expect(res.body).toEqual(newLocationObject);
+    done();
+  });
+});
 
-// // Start with posting a new location
-// describe("POST /api/location", () => {
-//   it("responds with json", async (done) => {
-//     const res = await request
-//       .post("/api/location")
-//       .send({
-//         id: 1234567,
-//         description: "test",
-//       })
-//       //.expect("Content-Type", /json/)
-//       .expect(201);
-// 			expect(res.body).toEqual(expect.objectContaining({id: 1234567}));
-//     done();
-//   });
-// });
+describe('GET /api/location/', () => {
+  it('Responds with an array of locations.', async (done) => {
+    const res = await adminUser
+      .get('/api/location')
+      .expect(200);
+    expect(res.body).toEqual(
+      expect.arrayContaining([newLocationObject])
+    );
+    done();
+  });
+});
 
-// describe("PUT /api/location/:id", () => {
-//   it("responds with json", async (done) => {
-//     const res = await request
-//       .put("/api/location/1234567")
-//       .send({
-//         id: 1234567,
-//         description: "test edited",
-//       })
-//       //.expect("Content-Type", /json/)
-//       .expect(200);
-//     expect(res.body).toEqual({
-//       id: 1234567,
-//       description: "test edited",
-//     });
-//     done();
-//   });
-// });
+describe(`DELETE /api/location/${locationID}`, () => {
+  it('Responds with status code 204 OK because a location was deleted.', async (done) => {
+    await adminUser
+      .delete(`/api/location/${locationID}`)
+      .expect(204);
+    done();
+  });
+});
 
-// describe("GET /api/location/", () => {
-//   it("responds with json", async (done) => {
-//     const res = await request
-//       .get("/api/location")
-//       .set("Accept", "application/json")
-//       // .expect("Content-Type", /json/)
-// 			.expect(200);
-
-// 		expect(res.body).toEqual(
-// 			expect.arrayContaining([expect.objectContaining({
-// 			id: 1234567,
-// 			description: "test edited",
-// 		})])
-// 		);
-		
-// 		done();
-//   });
-// });
-
-// describe("DELETE /api/location/id", () => {
-// 	it("responds with status code 204 OK", async (done) => {
-// 		const res = await request
-// 		.delete("/api/location/1234567")
-// 		.expect(204);
-// 	done();
-// 	})
-// })
+describe('GET /api/location/', () => {
+  it('Responds with an array of locations.', async (done) => {
+    const res = await adminUser
+      .get('/api/location')
+      .expect(200);
+    expect(res.body).not.toEqual(
+      expect.arrayContaining([newLocationObject])
+    );
+    done();
+  });
+});
