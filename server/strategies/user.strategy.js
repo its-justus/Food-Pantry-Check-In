c89d023603ff2dc75ef52742670e7a7e81256255
Luke Johnson <lukejohnson3712@gmail.com>
2020-08-14 19:54:22 -0500

    Close #168.

--- server/strategies/user.strategy.js
+++ server/strategies/user.strategy.js
@@ -7,32 +7,43 @@ passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
-passport.deserializeUser((id, done) => {
-  pool.query(`SELECT account.id, account."name", account.email, account.access_level,
-              profile.household_id, profile.latest_order FROM account
-              LEFT JOIN profile ON account.id = profile.account_id
-              WHERE account.id = $1;`, [id])
-    .then((result) => {
-      // Handle Errors
-      const user = result && result.rows && result.rows[0];
+passport.deserializeUser(async (id, done) => {
+  const conn = await pool.connect();
+  try {
+    // Get the various attributes about a ussr.
+    const getUserInfoQuery = {};
+    getUserInfoQuery.text = `SELECT account.id, account."name", account.email, account.access_level,
+                            profile.household_id, profile.latest_order FROM account
+                            LEFT JOIN profile ON account.id = profile.account_id
+                            WHERE account.id = $1;`;
+    getUserInfoQuery.values = [id];
+    await conn.query('BEGIN');
+    const result = await conn.query(getUserInfoQuery.text, getUserInfoQuery.values);
+    const user = result && result.rows && result.rows[0];
 
-      if (user) {
-        // user found
-        delete user.password; // remove password so it doesn't get sent
-        // done takes an error (null in this case) and a user
-        done(null, user);
-      } else {
-        // user not found
-        // done takes an error (null in this case) and a user (also null in this case)
-        // this will result in the server returning a 401 status code
-        done(null, null);
-      }
-    }).catch((error) => {
-      console.log('Error with query during deserializing user ', error);
-      // done takes an error (we have one) and a user (null in this case)
-      // this will result in the server returning a 500 status code
-      done(error, null);
-    });
+    // Do a second query that updates the user's profile to include the latest order object.
+    const getLatestOrderObjectQuery = {};
+    getLatestOrderObjectQuery.text = 'SELECT * FROM "order" WHERE id = $1;';
+    getLatestOrderObjectQuery.values = [user.latest_order];
+    const orderRow = await conn.query(getLatestOrderObjectQuery.text, getLatestOrderObjectQuery.values);
+    await conn.query('COMMIT');
+    conn.release();
+
+    if (user) {
+      // Remove the user's password so it doesn't get sent.
+      delete user.password;
+      // done takes an error (null in this case) and a user.
+      done(null, { ...user, latest_order: orderRow.rows && orderRow.rows[0] });
+    } else {
+      // User not found.
+      // done takes an error (null in this case) and a user (also null in this case).
+      // This will result in the server returning a 401 status code.
+      done(null, null);
+    }
+  } catch (error) {
+    conn.query('ROLLBACK');
+    console.log('Error getting user information.', error);
+  }
 });
 
 // Does actual work of logging in
