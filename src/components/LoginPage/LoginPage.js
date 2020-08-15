8d24d176170c4d231cfcd522218c76f58d58c64a
Bailey Niemiller <baileyniemiller@gmail.com>
2020-08-15 10:10:21 -0500

    remove warnings

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -7,7 +7,6 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import Toast from "react-bootstrap/Toast";
-import Footer from "../Footer/Footer";
 import "./LoginPage.css";
 
 class LoginPage extends Component {
