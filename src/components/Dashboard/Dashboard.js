10eed212221d417e3325fa169aff27841198ccb0
Daniel Simango <daniel.simmz@gmail.com>
2020-08-17 09:17:17 -0500

    tried making form appear on click

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -93,81 +93,86 @@ class Dashboard extends Component {
                 </form>
               </Card>
             </Col>
-            <Col id="secondCol" xs={12} sm={12} lg={4} xl={4}>
-              <Card id="secondCard">
-                {this.state.showClientInfo ? (
-                  <form className="dashForm">
-                    <div id="secondColHeader">
-                      <h1 id="secondColTitle">Client Information</h1>
-                      <button
-                        disabled={this.state.orderObj.checkout_at}
-                        id="checkInClient"
-                        className="btn btn-large btn-primary"
-                        onClick={() =>
-                          this.props.dispatch({
-                            type: "ORDER_CHECKOUT",
-                            payload: {
-                              id: this.state.orderObj.id,
-                              waitTimeMinutes: this.state.waitTimeMinutes,
-                            },
-                          })
-                        }
-                      >
-                        Check In
-                      </button>
-                    </div>
-                    <body id="dashBody">
-                      <h3>Name: {this.state.orderObj.name}</h3>
-                      <h4>Household ID: {this.state.orderObj.account_id}</h4>
-                      <br />
-                      <p className="clientInformation">
-                        Walking home: <b>{this.state.orderObj.walking_home}</b>
-                      </p>
-                      <p className="clientInformation">
-                        Child Birthday coming up:{" "}
-                        <b>{this.state.orderObj.child_birthday}</b>
-                      </p>
-                      <p className="clientInformation">
-                        Someone at home is pregnant:{" "}
-                        <b>{this.state.orderObj.pregnant}</b>
-                      </p>
-                      <p className="clientInformation">
-                        Dietary restrictions:{" "}
-                        <b>{this.state.orderObj.dietary_restrictions}</b>
-                      </p>
-                      <p className="clientInformation">
-                        SNAP: <b>{this.state.orderObj.snap}</b>
-                      </p>
-                      <p className="clientInformation">
-                        Other needs: <b>{this.state.orderObj.other}</b>
-                      </p>
-                    </body>
-                    <label for="waitTime">
-                      Please choose a wait time:
-                      <select
-                        name="waitTime"
-                        id="times"
-                        value={this.state.waitTimeMinutes}
-                        onChange={(event) =>
-                          this.setState({
-                            waitTimeMinutes: event.target.value,
-                          })
-                        }
-                      >
-                        <option value="15">15 minutes</option>
-                        <option value="30">30 minutes</option>
-                        <option value="45">45 minutes</option>
-                        <option value="60">60 minutes</option>
-                      </select>
-                    </label>
-                  </form>
-                ) : (
-                  <ManualOrder
-                    toggleShowClientInfo={this.toggleShowClientInfo}
-                  />
-                )}
-              </Card>
-            </Col>
+            {this.state.orderObj === {} ? (
+              <span></span>
+            ) : (
+              <Col id="secondCol" xs={12} sm={12} lg={4} xl={4}>
+                <Card id="secondCard">
+                  {this.state.showClientInfo ? (
+                    <form className="dashForm">
+                      <div id="secondColHeader">
+                        <h1 id="secondColTitle">Client Information</h1>
+                        <button
+                          disabled={this.state.orderObj.checkout_at}
+                          id="checkInClient"
+                          className="btn btn-large btn-primary"
+                          onClick={() =>
+                            this.props.dispatch({
+                              type: "ORDER_CHECKOUT",
+                              payload: {
+                                id: this.state.orderObj.id,
+                                waitTimeMinutes: this.state.waitTimeMinutes,
+                              },
+                            })
+                          }
+                        >
+                          Check In
+                        </button>
+                      </div>
+                      <body id="dashBody">
+                        <h3>Name: {this.state.orderObj.name}</h3>
+                        <h4>Household ID: {this.state.orderObj.account_id}</h4>
+                        <br />
+                        <p className="clientInformation">
+                          Walking home:{" "}
+                          <b>{this.state.orderObj.walking_home}</b>
+                        </p>
+                        <p className="clientInformation">
+                          Child Birthday coming up:{" "}
+                          <b>{this.state.orderObj.child_birthday}</b>
+                        </p>
+                        <p className="clientInformation">
+                          Someone at home is pregnant:{" "}
+                          <b>{this.state.orderObj.pregnant}</b>
+                        </p>
+                        <p className="clientInformation">
+                          Dietary restrictions:{" "}
+                          <b>{this.state.orderObj.dietary_restrictions}</b>
+                        </p>
+                        <p className="clientInformation">
+                          SNAP: <b>{this.state.orderObj.snap}</b>
+                        </p>
+                        <p className="clientInformation">
+                          Other needs: <b>{this.state.orderObj.other}</b>
+                        </p>
+                      </body>
+                      <label for="waitTime">
+                        Please choose a wait time:
+                        <select
+                          name="waitTime"
+                          id="times"
+                          value={this.state.waitTimeMinutes}
+                          onChange={(event) =>
+                            this.setState({
+                              waitTimeMinutes: event.target.value,
+                            })
+                          }
+                        >
+                          <option value="15">15 minutes</option>
+                          <option value="30">30 minutes</option>
+                          <option value="45">45 minutes</option>
+                          <option value="60">60 minutes</option>
+                        </select>
+                      </label>
+                    </form>
+                  ) : (
+                    <ManualOrder
+                      toggleShowClientInfo={this.toggleShowClientInfo}
+                    />
+                  )}
+                </Card>
+              </Col>
+            )}
             <Col id="thirdCol" xs={12} sm={12} lg={4} xl={4}>
               <Card id="thirdCard">
                 <form className="dashForm">
